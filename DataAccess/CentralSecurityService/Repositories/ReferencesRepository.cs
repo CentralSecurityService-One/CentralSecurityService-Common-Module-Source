1275830471244d11fab03f24d65231b2ff45d7b5
Eamonn Duffy <Eamonn@Duffy.global>
2025-06-15 22:19:34 +0100

    feat: Cosmetic indentation change.

--- DataAccess/CentralSecurityService/Repositories/ReferencesRepository.cs
+++ DataAccess/CentralSecurityService/Repositories/ReferencesRepository.cs
@@ -21,7 +21,7 @@ namespace CentralSecurityService.Common.DataAccess.CentralSecurityService.Reposi
             // This will be translated to T-SQL SUBSTRING in SQL Server.
             return Database.References
                 .Any(referenceEntity => referenceEntity.ReferenceName.Length > 17 &&
-                          referenceEntity.ReferenceName.Substring(17, sourceReferenceName.Length + 17) == sourceReferenceName);
+                     referenceEntity.ReferenceName.Substring(17, sourceReferenceName.Length + 17) == sourceReferenceName);
         }
     }
 }
