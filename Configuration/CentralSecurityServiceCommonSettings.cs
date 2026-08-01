ec2b4c4c1c72686dea0f5cb7f040b85b3d394f32
Eamonn Duffy <Eamonn.Duffy@GMail.com>
2026-08-01 18:38:50 +0100

    feat: First phase of adding some PostgreSQL support.

--- Configuration/CentralSecurityServiceCommonSettings.cs
+++ Configuration/CentralSecurityServiceCommonSettings.cs
@@ -13,23 +13,6 @@ namespace CentralSecurityService.Common.Configuration
             Instance = this;
         }
 
-        public class DatabaseSettings
-        {
-            public string DatabaseServer { get; set; }
-
-            public string DatabaseName { get; set; }
-
-            public string DatabaseSchema { get; set; }
-
-            public string ApplicationName { get; set; }
-
-            public string UserName { get; set; }
-
-            public string Password { get; set; }
-
-            public string ConnectionString => $"Server={DatabaseServer};Database={DatabaseName};Application Name={ApplicationName};User Id={UserName};Password={Password};Encrypt=false;";
-        }
-
         public class GoogleReCaptchaSettings
         {
             public string SiteKey { get; set; }
@@ -56,8 +39,6 @@ namespace CentralSecurityService.Common.Configuration
                 return _databaseTypeValue.GetValueOrDefault();
             }
         }
-
-        public DatabaseSettings Database { get; set; }
 
         public SqlServerDatabaseSettings SqlServerDatabase { get; set; }
 
